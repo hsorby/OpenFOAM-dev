2150b9da91db64c49ced400d3970d05754172903
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-07-04 16:33:23 +1200

    Rename case cloudPascal.

--- src/rename.sh
+++ src/rename.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+p=$2
 t=$1
 q=`echo $t |cut -c1 |tr [:lower:] [:upper:]``echo ${t:1}`
-echo $t
-echo $q
-mv OpenFOAM/includePrivate/${t}.H tmp.H
+echo "$p/$t"
+echo "$p/$q"
+mv $p/${t}.H tmp.H
 
 git add .
 git ci -m "Rename case $t."
 
-mv tmp.H OpenFOAM/includePrivate/${q}.H
+mv tmp.H $p/${q}.H
 
 git add .
 git ci -m "Rename case return $q."
