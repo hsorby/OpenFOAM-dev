62c2344afc488e9636d2f6499c93f273355dee51
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-07-04 16:28:40 +1200

    Rename case diagonalPreconditionerPascal.

--- src/rename.sh
+++ src/rename.sh
@@ -4,13 +4,13 @@ t=$1
 q=`echo $t |cut -c1 |tr [:lower:] [:upper:]``echo ${t:1}`
 echo $t
 echo $q
-mv OpenFOAM/includePrivate/${q}.H tmp.H
+mv OpenFOAM/includePrivate/${t}.H tmp.H
 
 git add .
 git ci -m "Rename case $t."
 
-mv tmp.H OpenFOAM/includePrivate/${t}.H
+mv tmp.H OpenFOAM/includePrivate/${q}.H
 
 git add .
-git ci -m "Rename case return $t."
+git ci -m "Rename case return $q."
 
